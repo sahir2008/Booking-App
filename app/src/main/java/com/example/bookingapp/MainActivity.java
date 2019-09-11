edbe3e9e7e6dc6a9ac065c68ac82251389ef09ac
belalgamal46 <32738452+belalgamal46@users.noreply.github.com>
2019-09-12 01:11:03 +0200

    nothing changed

--- app/src/main/java/com/example/bookingapp/MainActivity.java
+++ app/src/main/java/com/example/bookingapp/MainActivity.java
@@ -2,8 +2,11 @@ package com.example.bookingapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.graphics.BlurMaskFilter;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.jgabrielfreitas.core.BlurImageView;
 
