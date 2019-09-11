f94c8583a15f5237c264bb620320b3e4c064332f
Sahir Mohsen <sahirmoshen@gmail.com>
2019-09-12 00:57:32 +0200

    back arrow

--- app/src/main/java/com/example/bookingapp/MainActivity.java
+++ app/src/main/java/com/example/bookingapp/MainActivity.java
@@ -2,13 +2,17 @@ package com.example.bookingapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.graphics.BlurMaskFilter;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.jgabrielfreitas.core.BlurImageView;
 
 public class MainActivity extends AppCompatActivity {
     BlurImageView blur_bg;
+    Button sign_up_btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,6 +20,15 @@ public class MainActivity extends AppCompatActivity {
 
         blur_bg = (BlurImageView) findViewById(R.id.login_bg);
         blur_bg.setBlur(25);
+        sign_up_btn = findViewById(R.id.sign_up);
+
+        sign_up_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final Intent intent = new Intent(MainActivity.this,signUp_page.class);
+                startActivity(intent);
+            }
+        });
 
     }
 }
--- app/src/main/java/com/example/bookingapp/signUp_page.java
+++ app/src/main/java/com/example/bookingapp/signUp_page.java
@@ -2,12 +2,15 @@ package com.example.bookingapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 import com.jgabrielfreitas.core.BlurImageView;
 
 public class signUp_page extends AppCompatActivity {
-
+    ImageButton back_arrow_sign_up_page;
     BlurImageView blur_bg;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +19,15 @@ public class signUp_page extends AppCompatActivity {
 
         blur_bg = (BlurImageView) findViewById(R.id.login_bg);
         blur_bg.setBlur(25);
+
+        back_arrow_sign_up_page = findViewById(R.id.back_arrow_sign_up);
+
+        back_arrow_sign_up_page.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(signUp_page.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
