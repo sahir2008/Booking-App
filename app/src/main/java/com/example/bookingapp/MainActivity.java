c3b67a4984d3cdd16487fc809011d77bc2240fb0
Sahir Mohsen <sahirmoshen@gmail.com>
2019-09-12 15:00:11 +0200

    defined id widget of login_page and sign_up_page

--- app/src/main/java/com/example/bookingapp/MainActivity.java
+++ app/src/main/java/com/example/bookingapp/MainActivity.java
@@ -7,25 +7,49 @@ import android.graphics.BlurMaskFilter;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 import com.jgabrielfreitas.core.BlurImageView;
 
 public class MainActivity extends AppCompatActivity {
-    BlurImageView blur_bg;
-    Button sign_up_btn;
+    Button sign_up_btn, login_btn;
+    EditText email_edit_txt_login, pass_edit_txt_login;
+
+    DatabaseReference database = FirebaseDatabase.getInstance().getReference();
+    FirebaseAuth auth = FirebaseAuth.getInstance();
+    StorageReference storageReference = FirebaseStorage.getInstance().getReference();
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        blur_bg = (BlurImageView) findViewById(R.id.login_bg);
+        BlurImageView blur_bg = (BlurImageView) findViewById(R.id.login_bg);
         blur_bg.setBlur(25);
+
         sign_up_btn = findViewById(R.id.sign_up);
+        login_btn = findViewById(R.id.login_btn);
+        email_edit_txt_login = findViewById(R.id.email_login_edit_txt);
+        pass_edit_txt_login = findViewById(R.id.pass_login_edit_txt);
+
+        login_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final Intent intent = new Intent(MainActivity.this, Home_Page.class);
+                startActivity(intent);
+            }
+        });
 
         sign_up_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                final Intent intent = new Intent(MainActivity.this,signUp_page.class);
+                final Intent intent = new Intent(MainActivity.this, signUp_page.class);
                 startActivity(intent);
             }
         });
--- app/src/main/java/com/example/bookingapp/signUp_page.java
+++ app/src/main/java/com/example/bookingapp/signUp_page.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
 
 import com.jgabrielfreitas.core.BlurImageView;
@@ -12,6 +14,9 @@ import com.jgabrielfreitas.core.BlurImageView;
 public class signUp_page extends AppCompatActivity {
     ImageButton back_arrow_sign_up_page;
     BlurImageView blur_bg;
+    EditText user_name, user_pass, user_re_pass, user_email, user_phone_number;
+    Button register_btn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,12 +25,18 @@ public class signUp_page extends AppCompatActivity {
         blur_bg = (BlurImageView) findViewById(R.id.login_bg);
         blur_bg.setBlur(25);
 
-        back_arrow_sign_up_page = findViewById(R.id.back_arrow_sign_up);
+        user_name = findViewById(R.id.username_signUp);
+        user_pass = findViewById(R.id.password_signUp);
+        user_re_pass = findViewById(R.id.re_password_signUp);
+        user_email = findViewById(R.id.email_signUp);
+        user_phone_number = findViewById(R.id.phoneNumber_signUp);
+        register_btn = findViewById(R.id.btnRegister_signUp);
 
+        back_arrow_sign_up_page = findViewById(R.id.back_arrow_sign_up);
         back_arrow_sign_up_page.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(signUp_page.this,MainActivity.class);
+                Intent intent = new Intent(signUp_page.this, MainActivity.class);
                 startActivity(intent);
             }
         });
